d98c3d31d2028748ab9f81f89882280831319520
Gokul Karthic <gokulkarthic06@gmail.com>
2026-02-16 13:19:04 +0530

    Add files via upload

---
+++ UglyNumber.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class UglyNumber {
+      public static boolean isUgly(int n) {
+            if (n <= 0) {
+                  return false;
+            }
+            while (n > 1) {
+                  if (n % 2 == 0) {
+                        n /= 2;
+                  } else if (n % 3 == 0) {
+                        n /= 3;
+                  } else if (n % 5 == 0) {
+                        n /= 5;
+                  } else {
+                        return false;
+                  }
+            }
+            return true;
+
+      }
+
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            System.out.println(isUgly(n));
+      }
+
+}
