df9f2d00d5b999d4a971da6c0026a3a98c1d8dae
Gokul Karthic <gokulkarthic06@gmail.com>
2026-02-19 21:57:03 +0530

    Add files via upload

---
+++ PowerOf2.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+class PowerOf2 {
+      public static boolean isPowerOfTwo(int n) {
+            return n > 0 && (n & (n - 1)) == 0;
+
+      }
+
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            System.out.println(isPowerOfTwo(n));
+      }
+
+}
