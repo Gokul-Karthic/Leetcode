bdddcc78cb01fb1143aa6bd2a1171b957a233380
Gokul Karthic <gokulkarthic06@gmail.com>
2026-02-21 21:19:48 +0530

    Add files via upload

---
+++ LargestOddNumberinString.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class LargestOddNumberinString {
+
+      public static String largestOddNumber(String num) {
+
+            for (int i = num.length() - 1; i >= 0; i--) {
+                  if ((num.charAt(i) - '0') % 2 != 0) {
+                        return num.substring(0, i + 1);
+                  }
+
+            }
+            return "";
+      }
+
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String s = sc.next();
+            System.out.println(largestOddNumber(s));
+      }
+}
