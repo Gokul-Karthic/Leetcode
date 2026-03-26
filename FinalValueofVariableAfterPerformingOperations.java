7c78e018233bde16a87660b58f957e7af405923a
Gokul Karthic <gokulkarthic06@gmail.com>
2026-03-26 22:24:39 +0530

    Add files via upload

---
+++ FinalValueofVariableAfterPerformingOperations.java
@@ -0,0 +1,18 @@
+public class FinalValueofVariableAfterPerformingOperations {
+      public static int finalValueAfterOperations(String[] operations) {
+            int x = 0;
+            for (int i = 0; i < operations.length; i++) {
+                  if (operations[i].equals("++X")) {
+                        ++x;
+                  } else if (operations[i].equals("X++")) {
+                        x++;
+                  } else if (operations[i].equals("--X")) {
+                        --x;
+                  } else {
+
+                  }
+            }
+            return x;
+
+      }
+}
