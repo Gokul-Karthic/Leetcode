ab3f48ed6c86ce91c38b169dea3037e4658977f6
Gokul Karthic <gokulkarthic06@gmail.com>
2026-03-31 01:31:59 +0530

    Add files via upload

---
+++ NumberofGoodPairs.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NumberofGoodPairs {
+
+      public static int numIdenticalPairs(int[] nums) {
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                  for (int j = 0; j < nums.length; j++) {
+                        if (nums[i] == nums[j] && i < j) {
+                              count++;
+                        }
+                  }
+            }
+            return count;
+      }
+
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                  arr[i] = sc.nextInt();
+            }
+            System.out.println(numIdenticalPairs(arr));
+      }
+}
