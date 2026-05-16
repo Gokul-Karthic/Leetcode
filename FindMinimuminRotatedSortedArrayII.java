753632cb128daba0bc8bc2a1dca3754cb20b7773
Gokul Karthic <gokulkarthic06@gmail.com>
2026-05-16 22:07:09 +0530

    Add files via upload

---
+++ FindMinimuminRotatedSortedArrayII.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FindMinimuminRotatedSortedArrayII {
+      public static int findMin(int[] nums) {
+            int min = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                  if (nums[i] < min) {
+                        min = nums[i];
+                  }
+            }
+            return min;
+
+      }
+
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) {
+                  nums[i] = sc.nextInt();
+            }
+            System.out.println(findMin(nums));
+      }
+
+}
