50d230e677c57315f49132a8b5a10ba3c11052ca
Gokul Karthic <gokulkarthic06@gmail.com>
2026-06-05 22:50:54 +0530

    Add files via upload

---
+++ FindPivotNumber.java
@@ -0,0 +1,24 @@
+public class FindPivotNumber {
+      public int pivotInteger(int n) {
+            int left = 1;
+            int right = n;
+
+            int leftSum = 1;
+            int rightSum = n;
+
+            while (left < right) {
+                  if (leftSum < rightSum) {
+                        left++;
+                        leftSum += left;
+                  } else {
+                        right--;
+                        rightSum += right;
+                  }
+            }
+            if (leftSum == rightSum) {
+                  return left;
+            } else {
+                  return -1;
+            }
+      }
+}
