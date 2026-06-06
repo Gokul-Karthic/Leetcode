f4b774208e9f1b43fbd006fc5e81835729dd31ca
Gokul Karthic <gokulkarthic06@gmail.com>
2026-06-06 21:47:21 +0530

    Add files via upload

---
+++ CountingWordsWithaGivenPrefix.java
@@ -0,0 +1,12 @@
+public class CountingWordsWithaGivenPrefix {
+      public int prefixCount(String[] words, String pref) {
+            int count = 0;
+            int n = pref.length();
+            for (String s : words) {
+                  if (s.length() >= n && s.startsWith(pref)) {
+                        count++;
+                  }
+            }
+            return count;
+      }
+}
